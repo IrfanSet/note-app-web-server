206c2232fedbc360bdf687a3b8373b0f806ab18c
Irfan Setiawan <irfansettz@gmail.com>
2022-04-19 23:01:28 +0700

    add env

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const route = require('./route');
 
 const server = hapi.server({
     port : 5000,
-    host : 'localhost',
+    host : process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
         cors: {
             origin: ['*']         
