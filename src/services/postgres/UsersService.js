9e1eb96a882b7fa4657f8759359085c11bd69a66
Irfan Setiawan <irfansettz@gmail.com>
2022-05-04 21:39:07 +0700

    fix query

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -23,7 +23,7 @@ class UsersService {
         const id = `user-${nanoid(16)}`
         const hashedPassword = await bcrypt.hash(password, 10);
         const query = {
-            text: 'insert into users set($1, $2, $3, $4) returning id',
+            text: 'insert into users values($1, $2, $3, $4) returning id',
             values: [id, username, hashedPassword, fullname]
         }
 
